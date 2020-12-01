fe58eab0e61cd8e2098c294f77eb8623d2437f76
suryaprakash <surya.zvky@gmail.com>
2020-12-01 22:46:42 +0530

    added the post to firebase

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, FlatList, SafeAreaView} from 'react-native';
+import {Container, H1} from 'native-base';
 
-const Home = () => {
+import {connect} from 'react-redux';
+import {getPosts} from '../action/post';
+import propTypes from 'prop-types';
+
+import EmptyContainer from '../components/EmptyContainer';
+import Post from '../components/Post';
+
+const Home = ({getPosts, postState, userDetails}) => {
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  if (postState.isLoading) {
+    return <EmptyContainer />;
+  }
+
   return <Text>Hello from Home</Text>;
 };
 
-export default Home;
+export default connect()(Home);
--- src/utils/AskPermission.js
+++ src/utils/AskPermission.js
@@ -5,6 +5,7 @@ export const requestPermission = async () => {
     const granted = await PermissionsAndroid.requestMultiple([
       PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      PermissionsAndroid.PERMISSIONS.CAMERA,
     ]);
 
     console.log(granted);
