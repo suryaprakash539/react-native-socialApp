c0ffd517fd665178c23d397241b4373f145f79e8
suryaprakash <surya.zvky@gmail.com>
2020-12-01 11:52:46 +0530

    firebase subscription added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text} from 'react-native';
+import 'react-native-gesture-handler';
 
-const App = () => {
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {connect, useDispatch} from 'react-redux';
+
+import AddPost from './screens/AddPost';
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import Home from './screens/Home';
+import CustomHeader from './layout/CustomHeader';
+
+import {IS_AUTHENTICATED, SET_USER} from './action/action.types';
+import EmptyContainer from './components/EmptyContainer';
+import {requestPermission} from './utils/AskPermission';
+
+const Stack = createStackNavigator();
+
+const App = ({authState}) => {
+  const dispatch = useDispatch();
+
+  const onAuthStateChanged = (user) => {
+    if (user) {
+      dispatch({
+        type: IS_AUTHENTICATED,
+        payload: true,
+      });
+      console.log(user._user.uid);
+
+      database()
+        .ref(`/users/${user._user.uid}`)
+        .on('value', (snapshot) => {
+          console.log('USER DETAILS', snapshot.val());
+          dispatch({
+            type: SET_USER,
+            payload: snapshot.val(),
+          });
+        });
+    } else {
+      dispatch({
+        type: IS_AUTHENTICATED,
+        payload: false,
+      });
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unmounts the user
+  }, []);
+
   return <Text>Hello from App.js</Text>;
 };
 
