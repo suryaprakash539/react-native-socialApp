86e991deaaad64459dd8a33d3c56bfd6e45b1bfe
suryaprakash <surya.zvky@gmail.com>
2020-12-01 13:29:18 +0530

    signup form and image upload setup

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, ScrollView, TouchableOpacity, View} from 'react-native';
 
-const SignUp = () => {
+import {
+  Container,
+  Form,
+  Item,
+  Input,
+  Text,
+  Button,
+  Thumbnail,
+  Content,
+} from 'native-base';
+
+import storage from '@react-native-firebase/storage';
+import ProgressBar from 'react-native-progress/Bar';
+
+import ImagePicker from 'react-native-image-picker';
+import {options} from '../utils/options';
+
+import propTypes from 'prop-types';
+import {signUp} from '../action/auth';
+import {connect} from 'react-redux';
+
+const SignUp = ({signUp}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [instaUserName, setInstaUserName] = useState('');
+  const [country, setCountry] = useState('');
+  const [bio, setBio] = useState('');
+  const [image, setImage] = useState(
+    'https://firebase.google.com/brand-guidelines',
+  );
+  const [imageUploading, setImageUploading] = useState(false);
+  const [uploadStatus, setUploadStatus] = useState(null);
+
   return <Text>Hello from SignUp</Text>;
 };
 
-export default SignUp;
+const mapDispatchToProps = {
+  signUp: (data) => signUp(data),
+};
+
+SignUp.propTypes = {
+  signUp: propTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(SignUp);
